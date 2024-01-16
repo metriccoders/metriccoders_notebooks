b1c4bbbcb93aefcb6bca5b471ab5511f3c73acfb
Metric Coders <techsaadhana@gmail.com>
2024-01-16 14:41:52 +0100

    added notebooks

---
+++ pipelines/Notebook38.py
@@ -0,0 +1,17 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import HistGradientBoostingRegressor
+
+
+X, y = make_regression(random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=56, test_size=0.2)
+
+pipe = Pipeline([
+    ("scaler", StandardScaler()),
+    ("clf", HistGradientBoostingRegressor())
+])
+
+scores = pipe.fit(X_train, y_train).score(X_test, y_test)
+print(scores)
---
+++ pipelines/Notebook39.py
@@ -0,0 +1,17 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import HuberRegressor
+
+
+X, y = make_regression(random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=56, test_size=0.2)
+
+pipe = Pipeline([
+    ("scaler", StandardScaler()),
+    ("clf", HuberRegressor())
+])
+
+scores = pipe.fit(X_train, y_train).score(X_test, y_test)
+print(scores)
