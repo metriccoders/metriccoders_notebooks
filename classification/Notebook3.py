28562c046b81cf14ce9828cff88c305f91193911
Metric Coders <techsaadhana@gmail.com>
2024-02-09 21:48:05 +0100

    added print statements

--- classification/Notebook3.py
+++ classification/Notebook3.py
@@ -6,10 +6,13 @@ from sklearn.gaussian_process import GaussianProcessClassifier
 iris = load_iris()
 X = iris.data
 y = iris.target
+print("Data:", X[:10])
+print("Target:", y[:10])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 clf = GaussianProcessClassifier()
 clf.fit(X_train, y_train)
+
 y_pred = clf.predict(X_test)
 
 print(accuracy_score(y_test, y_pred))
--- crossvalmetrics/Notebook3.py
+++ crossvalmetrics/Notebook3.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import cross_val_score, ShuffleSplit
 
 dataset = load_iris()
 X, y = dataset.data, dataset.target
+print("Data:", X)
+print("Target:", y)
 
 clf = AdaBoostClassifier(
     n_estimators=6
